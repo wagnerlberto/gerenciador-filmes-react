e9882ca71000a7f97cde72ce21df9828062fb2c8
Wagner L. Berto <wagnerlberto@hotmail.com>
2024-12-27 12:57:59 -0300

    Adição de logo responsivo

--- src/components/topbanner/index.jsx
+++ src/components/topbanner/index.jsx
@@ -1,9 +1,11 @@
+import TesteLogo from "./imgs/logo.jpeg";
 import "./style.css";
 
 export function Topbanner(){
   return(
     <div className="topbanner">
       <center className="topbanner">
+        <img className="topbanner" src={TesteLogo} alt="Logo" />
         GERENCIADOR DE FILMES
       </center>
     </div>
