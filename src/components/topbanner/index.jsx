081c6296ce0754c1207cd0ee54bf79100532bb67
Wagner L. Berto <wagnerlberto@hotmail.com>
2024-12-27 12:27:58 -0300

    Criação do título

---
+++ src/components/topbanner/index.jsx
@@ -0,0 +1,11 @@
+import "./style.css";
+
+export function Topbanner(){
+  return(
+    <div className="topbanner">
+      <center className="topbanner">
+        GERENCIADOR DE FILMES
+      </center>
+    </div>
+  );
+}
