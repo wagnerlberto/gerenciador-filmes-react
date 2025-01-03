452c6ffa06bedbd04327d80ec29e148ed860cb6f
Wagner L. Berto <wagnerlberto@hotmail.com>
2025-01-03 13:07:03 -0300

    Mostrando os diretores e filmes

--- src/pages/directors/index.jsx
+++ src/pages/directors/index.jsx
@@ -1,10 +1,46 @@
 import "./style.css";
+import { BACKEND_URL } from "../../config/index.js";
+import axios from "axios";
+import { useState, useEffect } from "react";
+
+function directorsHandler(directors){
+  if(!Array.isArray(directors)) 
+    return directors;
+
+  let i = 1;
+  let content = directors.map(director => {
+    return (
+      <div className="directors-content" key={i++}>
+        <h2>{director.nome}</h2>
+      </div>
+    );
+  });
+
+  return content;
+}
 
 export function Directors() {
+
+  const [directors, setDirectors] = useState("Nada a mostrar");
+
+  useEffect(() => {
+    axios.get(`${BACKEND_URL}/diretores`)
+    .then(response => {
+      let directors = response.data;
+      setDirectors(directorsHandler(directors));
+    })
+    .catch(error => {
+      console.error(error);
+    })
+    .finally(() => {
+      console.log("Finally");
+    });
+  }, []);
+
   return (
     <div className="content">
       <h1>DIRETORES</h1>
-      <p>CONTEÚDO</p>
+      {directors}
     </div>
   );
 }
--- src/pages/films/index.jsx
+++ src/pages/films/index.jsx
@@ -1,10 +1,46 @@
 import "./style.css";
+import { BACKEND_URL } from "../../config/index.js";
+import axios from "axios";
+import { useState, useEffect } from "react";
+
+function filmsHandler(films){
+  if(!Array.isArray(films)) 
+    return films;
+
+  let i = 1;
+  let content = films.map(film => {
+    return (
+      <div className="films-content" key={i++}>
+        <h2>{film.nome}</h2>
+      </div>
+    );
+  });
+
+  return content;
+}
 
 export function Films() {
+
+  const [films, setFilms] = useState("Nada a mostrar");
+
+  useEffect(() => {
+    axios.get(`${BACKEND_URL}/filmes`)
+    .then(response => {
+      let films = response.data;
+      setFilms(filmsHandler(films));
+    })
+    .catch(error => {
+      console.error(error);
+    })
+    .finally(() => {
+      console.log("Finally");
+    });
+  }, []);
+
   return (
     <div className="content">
       <h1>FILMES</h1>
-      <p>CONTEÚDO</p>
+      {films}
     </div>
   );
 }
