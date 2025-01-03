2f60763ce8f855d50eb243432b35e79208d4fb69
Wagner L. Berto <wagnerlberto@hotmail.com>
2025-01-03 12:30:09 -0300

    Utilizando useState

--- src/pages/actors/index.jsx
+++ src/pages/actors/index.jsx
@@ -1,14 +1,43 @@
 import "./style.css";
-import { BACKEND_URL } from "../../config";
+import { BACKEND_URL } from "../../config/index.js";
+import axios from "axios";
+import { useState } from "react";
+
+function actorsHandler(actors){
+  if(!Array.isArray(actors)) 
+    return actors;
+
+  let content = actors.map(actor => {
+    return (
+      <div className="actor">
+        <h2>{actor.nome}</h2>
+      </div>
+    );
+  });
+
+  return content;
+}
 
 export function Actors() {
 
-  console.log("Backend: " + BACKEND_URL);
+  const [actors, setActors] = useState("Nada a mostrar");
+
+  axios.get(`${BACKEND_URL}/atores`)
+    .then(response => {
+      let actors = response.data;
+      setActors(actorsHandler(actors));
+    })
+    .catch(error => {
+      console.error(error);
+    })
+    .finally(() => {
+      console.log("Finally");
+    });
 
   return (
     <div className="content">
       <h1>ATORES</h1>
-      <p>CONTEÚDO</p>
+      {actors}
     </div>
   );
 }
