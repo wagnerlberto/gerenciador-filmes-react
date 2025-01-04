abcf6cc0bfa987842ef247ba8a81bdb3239df2d5
Wagner L. Berto <wagnerlberto@hotmail.com>
2025-01-03 23:16:33 -0300

    Início do formulário de cadastro de filmes

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -4,7 +4,23 @@ export function Home() {
   return (
     <div className="content">
       <h1>HOME</h1>
-      <p>CONTEÚDO</p>
+      <div className="home-form">
+        <h3 className="home-form">Inserir novo filme:</h3>
+        <p className="home-form">
+          <input type="text" placeholder="Nome do filme" className="home-form" size="40"/>
+        </p>
+        <p className="home-form">
+          <input type="text" placeholder="Nome do diretor" className="home-form" size="40" />
+        </p>
+        <p className="home-form">
+          <input type="number" placeholder="Nota do filme" className="home-form" size="10" />
+        </p>
+        <h3 className="home-form">Elenco:</h3>
+        <p className="home-form">
+          <input type="text" placeholder="Nome do ator" className="home-form" size="40" />
+        </p>
+        
+      </div>
     </div>
   );
 }
